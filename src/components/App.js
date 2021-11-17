07e3e51c96a7eed7c6b57b622cc36dd8584ba51d
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-17 16:18:36 -0300

    Create a user context

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
+import { useState } from 'react';
+import UserContext from '../contexts/UserContexts.js';
+
 export default function App() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+
   return (
-    <>
-    </>
+    <UserContext.Provider value={{ user, setUser }}>
+    </UserContext.Provider>
   );
 }
