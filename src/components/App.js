2216e3ecfa2285497519c93c97cdf2372b6ef219
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-17 16:33:01 -0300

    Use react router dom and now the Home component is a route

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import { useState } from 'react';
 import UserContext from '../contexts/UserContexts.js';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Home.js';
 
 export default function App() {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
 
     return (
-      <UserContext.Provider value={{ user, setUser }}>
-        <Home />
-      </UserContext.Provider>
+        <UserContext.Provider value={{ user, setUser }}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/home" component={Home} exact />
+                </Switch>
+            </BrowserRouter>
+        </UserContext.Provider>
     );
 }
