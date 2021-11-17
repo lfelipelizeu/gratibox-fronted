b7f318cc0b1c440d01ff6d3c6415f6ea1bb057fd
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-17 16:27:31 -0300

    Add the Home component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import UserContext from '../contexts/UserContexts.js';
+import Home from './Home.js';
 
 export default function App() {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
 
-  return (
-    <UserContext.Provider value={{ user, setUser }}>
-    </UserContext.Provider>
-  );
+    return (
+      <UserContext.Provider value={{ user, setUser }}>
+        <Home />
+      </UserContext.Provider>
+    );
 }
