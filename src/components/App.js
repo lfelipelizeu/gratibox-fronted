33a543a072c87e381ce7f2170caa2abe01ea8094
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-22 02:29:53 -0300

    Create a new context

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import UserContext from '../contexts/UserContexts.js';
+import NewSubscriptionContext from '../contexts/NewSubscriptionContext.js';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import GlobalStyle from '../styles/GlobalStyle.js';
 import Home from './Home.js';
@@ -10,19 +11,22 @@ import Subscribe from './Subscribe/Subscribe.js';
 
 export default function App() {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+    const [newSubscription, setNewSubscription] = useState();
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
-            <BrowserRouter>
-            <GlobalStyle />
-                <Switch>
-                    <Route path='/home' component={Home} exact />
-                    <Route path='/signup' component={SignUp} exact />
-                    <Route path='/login' component={LogIn} exact />
-                    <Route path='/' component={ViewPlan} exact />
-                    <Route path='/subscribe/:plan' component={Subscribe} exact />
-                </Switch>
-            </BrowserRouter>
+            <NewSubscriptionContext.Provider value={{ newSubscription, setNewSubscription}}>
+                <BrowserRouter>
+                <GlobalStyle />
+                    <Switch>
+                        <Route path='/home' component={Home} exact />
+                        <Route path='/signup' component={SignUp} exact />
+                        <Route path='/login' component={LogIn} exact />
+                        <Route path='/' component={ViewPlan} exact />
+                        <Route path='/subscribe/:plan' component={Subscribe} exact />
+                    </Switch>
+                </BrowserRouter>
+            </NewSubscriptionContext.Provider>
         </UserContext.Provider>
     );
 }
